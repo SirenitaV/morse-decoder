0c7e11fe1a39860b30ebd5014eb5669527b2558a
SirenitaV <sirenita.violetta@gmail.com>
2022-03-10 12:50:43 +0500

    feat: solution for the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let zeroOneExpr = '';
+    let morseExpr = '';
+    let decodedString = '';
+    for (let i = 0; i < expr.length; i + 1) {
+        let value = expr.substring(i, i + 10);
+        if(value !== '**********') {
+          zeroOneExpr = `${value.replace(/00/g, '')}`;
+          morseExpr = zeroOneExpr.replace(/10/g, '.');
+          morseExpr = morseExpr.replace(/11/g, '-');
+          decodedString = `${decodedString}${MORSE_TABLE[morseExpr]}`;
+        }
+        
+        else {
+          decodedString = `${decodedString} `;  
+        }        
+        
+        
+        //console.log(decodedString);
+        i = i + 10;
+        // for (let entry of MORSE_TABLE) {
+        //     console.log(entry);
+        // }
+        // for (let j = 0; j < Object.keys(MORSE_TABLE); j ++) {
+        //   if(MORSE_TABLE[j] === morseExpr) {
+        //       decodedString = `${decodedString}${MORSE_TABLE[j]}`;
+        //       //console.log(morseExpr)
+        //       console.log(MORSE_TABLE[j]);
+        //       j = j + 1;
+        //   }
+        //   else {
+        //      j = j + 1; 
+        //   }            
+        // }
+        
+        
+        
+    }
+    return decodedString;
+    
+    //console.log(Object.values(MORSE_TABLE));
+    
 }
 
 module.exports = {
